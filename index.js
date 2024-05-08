df468a740d5646bb002e411f863af7f966b46725
ttn1401 <120162206+ttn1401@users.noreply.github.com>
2024-05-08 22:16:42 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = require("cors");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const cookieParser = require ("cookie-parser");
-const app = express();
-
-app.listen 
